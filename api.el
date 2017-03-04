81536894bf59e6d1333c5f7799a5bbc964d9e29f
Josh Stupplebeen <jpstup@gmail.com>
2017-03-03 20:18:38 -0700

    api init

---
+++ api.el
@@ -0,0 +1,13 @@
+;;; package -- Summary
+
+;;; Commentary:
+
+;;; Code:
+;;; Imports
+(require 'request)
+
+;;; Constants
+(defun rally-url () "https://rally1.rallydev.com")
+(print (rally-url))
+
+;;; api.el ends here
